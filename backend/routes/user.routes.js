d61dacc31a82719b45826d2aab9a5b330f4ce557
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-06-16 17:01:57 +0200

    T_T

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
-//const uploadController = require("../controllers/upload.controller");
-//const multer = require("multer");
-//const upload = multer();
+const uploadController = require('../controllers/upload.controller');
+const multer = require("multer");
+const upload = multer();
 
 // auth
-//router.post("/register", authController.signUp);
-//router.post("/login", authController.signIn);
-//router.get("/logout", authController.logout);
+router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 // user DB
 router.get("/", userController.getAllUsers);
@@ -19,6 +19,6 @@ router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
 
 // upload
-//router.post("/upload", upload.single("file"), uploadController.uploadProfil);
+router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
