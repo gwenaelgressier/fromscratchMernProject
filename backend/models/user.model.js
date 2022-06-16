f78088f6d818809ef648b4e80d503f4c9abd8930
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-06-16 12:16:07 +0200

    avancer models controler route

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
     {
@@ -9,13 +10,13 @@ const userSchema = new mongoose.Schema(
             minLength: 3,
             maxLength: 55,
             unique: true,
-            trim: true, //espace suprimer en fin de remplissage
+            trim: true,
         },
         email: {
             type: String,
             required: true,
             validate: [isEmail],
-            lowercase: true, //minuscule obligatoire
+            lowercase: true,
             unique: true,
             trim: true,
         },
@@ -47,6 +48,25 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+// play function before save into display: 'block',
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error("incorrect password");
+    }
+    throw Error("incorrect email");
+};
 
 const UserModel = mongoose.model("user", userSchema);
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
+//const uploadController = require("../controllers/upload.controller");
+//const multer = require("multer");
+//const upload = multer();
 
-//sa va declencher la function signUP dans le dossier authController
-router.post("/register", authController.signUp);
+// auth
+//router.post("/register", authController.signUp);
+//router.post("/login", authController.signIn);
+//router.get("/logout", authController.logout);
+
+// user DB
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
+
+// upload
+//router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 
 module.exports = router;
